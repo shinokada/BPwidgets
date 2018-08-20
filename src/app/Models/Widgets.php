e33f30eab3f78abab3b7bc43d4cb527202152a84
Shin Okada <sokada@canacad.ac.jp>
2018-08-20 21:59:56 +0900

    slug fixed

--- src/app/Models/Widgets.php
+++ src/app/Models/Widgets.php
@@ -13,7 +13,7 @@ class Widgets extends Model
     use Sluggable;
     use SluggableScopeHelpers;
     protected $table = 'widgets';
-    protected $fillable = ['value'];
+    protected $fillable = ['title', 'slug', 'order', 'position', 'content'];
     /**
      * Return the sluggable configuration array for this model.
      *
@@ -26,43 +26,6 @@ class Widgets extends Model
                 'source' => 'slug_or_title',
             ],
         ];
-    }
-    /**
-     * Grab a widget value from the database.
-     *
-     * @param string $key The widget key, as defined in the key db column
-     *
-     * @return string The widget value.
-     */
-    public static function get($key)
-    {
-        $widget = new self();
-        $entry = $widget->where('key', $key)->first();
-        if (!$entry) {
-            return;
-        }
-        return $entry->value;
-    }
-    /**
-     * Update a widget's value.
-     *
-     * @param string $key   The widget key, as defined in the key db column
-     * @param string $value The new value.
-     */
-    public static function set($key, $value = null)
-    {
-        $prefixed_key = 'widgets.' . $key;
-        $widget = new self();
-        $entry = $widget->where('key', $key)->firstOrFail();
-        // update the value in the database
-        $entry->value = $value;
-        $entry->saveOrFail();
-        // update the value in the session
-        Config::set($prefixed_key, $value);
-        if (Config::get($prefixed_key) == $value) {
-            return true;
-        }
-        return false;
     }
 
     // The slug is created automatically from the "name" field if no slug exists.
