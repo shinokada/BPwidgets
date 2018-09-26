2b16c2e208a0e802486d7d873e31c027c65bb138
Shin Okada <sokada@canacad.ac.jp>
2018-09-26 17:56:50 +0900

    Model update

--- src/app/Models/Widgets.php
+++ src/app/Models/Widgets.php
@@ -3,30 +3,13 @@ namespace shinokada\BPwidgets\app\Models;
 
 use Backpack\CRUD\CrudTrait;
 //use Config;
-use Cviebrock\EloquentSluggable\Sluggable;
-use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 use Illuminate\Database\Eloquent\Model;
 
 class Widgets extends Model
 {
     use CrudTrait;
-    use Sluggable;
-    use SluggableScopeHelpers;
     protected $table = 'widgets';
     protected $fillable = ['value'];
-    /**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'slug_or_title',
-            ],
-        ];
-    }
     /**
      * Grab a widget value from the database.
      *
@@ -65,12 +48,4 @@ class Widgets extends Model
         return false;
     }
 
-    // The slug is created automatically from the "name" field if no slug exists.
-    public function getSlugOrTitleAttribute()
-    {
-        if ($this->slug != '') {
-            return $this->slug;
-        }
-        return $this->title;
-    }
 }
